83789f1bb1f439ad6f372e54be513da7c056f25f
daniel-li2021 <92133276+daniel-li2021@users.noreply.github.com>
2022-08-23 14:28:46 -0700

    Update heap.h

--- heap.h
+++ heap.h
@@ -1,23 +1,75 @@
 // heap.h
-// Student name: 
-//
+// Daniel Li 
 
 #ifndef HEAP_H
 #define HEAP_H
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 class Heap{
+   public:
+    // return size of the heap
+      unsigned int size() {
+         return vdata.size();
+      }
+   
+      void push(int value);     // insert value; //duplicates are allowed
+      void pop(); // delete the min element 
+      int top();
+      bool empty();
 
- public:
-    void push(int value);     // insert value; //duplicates are allowed
-    void pop(); // delete the min element 
-    int top();
-    bool empty();
-    
- private:
-    std::vector<int> vdata; //store the binary heap tree as a dynamic array
+   private:
+      std::vector<int> vdata; //store the binary heap tree as a dynamic array
+ 
+      int parent(int i) {
+         return (i - 1) / 2;
+      }
+ 
+      int left(int i) {
+         return (2*i + 1);
+      }
+ 
+      int right(int i) {
+         return (2*i + 2);
+      }
+ 
+
+    void heapdown(int i)
+    {
+        int leftnum = left(i);
+        int rightnum = right(i);
+        int min = i;
+ 
+        if (leftnum < size() && vdata[leftnum] < vdata[i]) {
+            min = leftnum;
+        }
+ 
+        if (rightnum < size() && vdata[rightnum] < vdata[min]) {
+            min = rightnum;
+        }
+ 
+        if (min != i)
+        {
+            std::swap(vdata[i], vdata[min]);
+            heapdown(min);
+        }
+    }
+ 
+    void heapup(int i)
+    {
+        if (i && vdata[parent(i)] > vdata[i])
+        {
+            std::swap(vdata[i], vdata[parent(i)]);
+            heapup(parent(i));
+        }
+    }
+ 
+
 };
+
+
+
 
 #endif
